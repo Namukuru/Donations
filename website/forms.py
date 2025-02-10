5313e1fe131c20d6ecd1488a2d9c0cf2abe84643
Namukuru Lauryn <namukurulauryn@gmail.com>
2025-02-10 23:04:29 +0300

    Adding labels

--- website/forms.py
+++ website/forms.py
@@ -4,11 +4,11 @@ from django import forms
 
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="", widget=forms.TextInput(
+    email = forms.EmailField(label="Email", widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
-    first_name = forms.CharField(label="", max_length="50", widget=forms.TextInput(
+    first_name = forms.CharField(label="First Name", max_length="50", widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'First Name'}))
-    last_name = forms.CharField(label="", max_length="50", widget=forms.TextInput(
+    last_name = forms.CharField(label="Last Name", max_length="50", widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
 
     class Meta:
@@ -21,15 +21,15 @@ class SignUpForm(UserCreationForm):
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'User Name'
-        self.fields['username'].label = ''
+        self.fields['username'].label = 'Username'
 
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['placeholder'] = 'Password'
-        self.fields['password1'].label = ''
+        self.fields['password1'].label = 'Password'
 
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
-        self.fields['password2'].label = ''
+        self.fields['password2'].label = 'Password'
 
         # Remove default help text
         for field_name in self.fields:
