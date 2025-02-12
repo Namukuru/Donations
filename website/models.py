e3b55e9846efc250ad36df8dfb4f5fa270a0cd3b
Namukuru Lauryn <namukurulauryn@gmail.com>
2025-02-12 16:58:30 +0300

    Addition of roles

--- website/models.py
+++ website/models.py
@@ -2,9 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+ROLE_CHOICES = [
+    ('donor', 'Donor'),
+    ('recipient', 'Recipient'),
+    ('admin', 'Admin'),
+]
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
-
+    role = models.CharField(max_length=10, choices = ROLE_CHOICES, default='donor')
+    
     def __str__(self):
         return f"{self.user.username}'s Profile"
