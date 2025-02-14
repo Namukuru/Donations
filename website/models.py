4409da53ce87c5120ccd35fb2058c78769ff27d3
Namukuru Lauryn <namukurulauryn@gmail.com>
2025-02-14 23:42:24 +0300

    Donate model

--- website/models.py
+++ website/models.py
@@ -13,4 +13,14 @@ class UserProfile(models.Model):
     role = models.CharField(max_length=10, choices = ROLE_CHOICES, default='donor')
     
     def __str__(self):
-        return f"{self.user.username}'s Profile"
+        return (f"{self.user.username}'s Profile")
+
+class Donation(models.Model):
+    donor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)  # Optional: Allow anonymous donations
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(blank=True, null=True)  # Optional: Allow donors to leave a message
+
+    def __str__(self):
+        return (f"Donation of ${self.amount} by {self.donor.username if self.donor else 'Anonymous'}")
