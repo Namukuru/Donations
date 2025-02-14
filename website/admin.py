434cbc4e48a35348667ecd8b7c7658d870ed46bc
Namukuru Lauryn <namukurulauryn@gmail.com>
2025-02-14 07:24:13 +0300

    Customizing the admin panel

--- website/admin.py
+++ website/admin.py
@@ -1,10 +1,23 @@
+# admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import UserProfile
 
-# Register your models here.
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role')  # Show role in the list view
-    search_fields = ('user__username', 'role')  # Allow search by role
-    list_filter = ('role',)  # Filter by role
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'User Profiles'
 
-admin.site.register(UserProfile, UserProfileAdmin)
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+    list_display = ('username', 'email', 'get_role', 'is_staff')
+    list_select_related = ('userprofile',)
+
+    def get_role(self, instance):
+        return instance.userprofile.role
+    get_role.short_description = 'Role'
+
+# Unregister the default User admin and register the custom one
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
