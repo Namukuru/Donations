f980c27d6c99d641dde8c928a1ab35efa3fda024
Namukuru Lauryn <namukurulauryn@gmail.com>
2025-02-18 10:22:13 +0300

    Addition of created at on Users

--- website/models.py
+++ website/models.py
@@ -11,7 +11,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
     role = models.CharField(max_length=10, choices = ROLE_CHOICES, default='donor')
-    
+    created_at = models.DateTimeField(auto_now_add=True)
+     
     def __str__(self):
         return (f"{self.user.username}'s Profile")
 
