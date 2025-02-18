72eaecc6da7c350930acd0e40c7cc48a4d15e3b2
Namukuru Lauryn <namukurulauryn@gmail.com>
2025-02-18 10:20:10 +0300

    Modifying the look of the admin donation table

--- website/admin.py
+++ website/admin.py
@@ -11,14 +11,25 @@ class UserProfileInline(admin.StackedInline):
 
 class CustomUserAdmin(UserAdmin):
     inlines = (UserProfileInline,)
-    list_display = ('username', 'email', 'get_role', 'is_staff')
+    list_display = ('id','username', 'email', 'get_role', 'is_staff', 'get_created_at')
     list_select_related = ('userprofile',)
 
     def get_role(self, instance):
         return instance.userprofile.role
     get_role.short_description = 'Role'
+    
+    def get_created_at(self, instance):
+        return instance.userprofile.created_at if instance.userprofile else "N/A"
+    get_created_at.short_description = "Created At"
+    
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'donor', 'amount','message', 'created_at')  # Columns displayed in the list view
+    list_filter = ('created_at',)  # Adds a filter on the right side
+    search_fields = ('donor_name',)  # Enables search by donor name
+    ordering = ('-created_at',)  # Orders by most recent donations first
+    readonly_fields = ('created_at',)  # Prevents editing the timestamp
 
 # Unregister the default User admin and register the custom one
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(Donation)
