cf858fc968c17fe5a64cb15c4140319e2ad6f876
Namukuru Lauryn <namukurulauryn@gmail.com>
2025-03-07 17:40:55 +0300

    Moving of authentication views

---
+++ website/auth_views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Log in successful!")
+            return redirect('loginUser')
+        else:
+            messages.success(request, "An error occurred. Please try again.")
+            return redirect('loginUser')
+    else:
+        return render(request, 'loginUser.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Log out successful!")
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            login(request, user)
+            messages.success(
+                request, "You have successfully registered! Welcome!")
+            return redirect('home')
+        else:
+            print(f"DEBUG: Form errors: {form.errors}")
+            messages.error(
+                request, "Registration failed. Please fix the errors.")
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
