7c6f99b3471445c30bce2705d551d8fb14475320
Namukuru Lauryn <namukurulauryn@gmail.com>
2025-03-11 14:23:37 +0300

    Refreshed .gitignore

--- static/js/base.js
+++
@@ -1,7 +0,0 @@
-//Message timing out: after 2 seconds
-setTimeout(function () {
-    var alertMessage = document.getElementById("alertMessage");
-    if (alertMessage) {
-      alertMessage.remove();
-    }
-  }, 2000);
