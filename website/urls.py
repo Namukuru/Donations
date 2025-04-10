c7a641dd2eb3e80852320dab5fa6d0d7a031e238
Namukuru Lauryn <namukurulauryn@gmail.com>
2025-04-10 13:02:06 +0300

    Customization of password reset email.

--- website/urls.py
+++ website/urls.py
@@ -42,8 +42,16 @@ urlpatterns = [
     path("needs/<int:pk>/delete/", need_delete, name="need_delete"),
     
     # Password reset links (built-in)
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset/', 
+         auth_views.PasswordResetView.as_view(
+             template_name="password_reset.html",
+             email_template_name='password_reset_email.txt',
+             html_email_template_name='password_reset_email.html',
+             subject_template_name='password_reset_subject.txt',
+             success_url='/password_reset/done/'), 
+         name='password_reset'
+         ),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name='password_reset_complete'),
 ]
